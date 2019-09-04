ae6df2850b0d0be3ef19753955bda25e001735ac
cheng <wangzhicheng@chelun.com>
2019-09-04 23:45:58 +0800

    create demo

---
+++ LottieTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LottieTestDemo
+//
+//  Created by cheng on 2019/8/31.
+//  Copyright © 2019年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-LottieTestDemo/Pods-LottieTestDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LottieTestDemo : NSObject
+@end
+@implementation PodsDummy_Pods_LottieTestDemo
+@end
